f021c669513a56ab2b76e65bf6ad5aaefeb25547
irakli85 <iberidze38@gmail.com>
2023-02-01 17:55:13 +0400

    exercise 1.1

--- src/App.js
+++ src/App.js
@@ -1,67 +1,39 @@
-const App = () => {  
-const course = 'Half Stack application development'
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
 
-const parts = [
-  {
-    name: 'Fundamentals of React: ',
-    exercises: 10
-  },
-
-  {
-    name: 'Using props to pass data: ',
-    exercises: 7
-  },
-
-  {
-    name: 'State of a component: ',
-    exercises: 14
-  }
-]
-
- const Header = ({course}) => {
+const Content = (props) => {
   return(
     <>
-      <h1>{course}</h1>
-    </>
-  )
- }
-
- const Part = ({arr}) =>{
-  return(
-    <>
-     <p>{arr.name}{arr.exercises}</p>
-    </>
-  )
- }
-
- const Content = () => {
-  return(
-    <>
-      <Part arr={parts[0]} />
-      <Part arr={parts[1]} />
-      <Part arr={parts[2]} />
-    </>
-  )
-  }
-
- const Total = () =>{
-  return(
-    
-    <>
-      <p>Numbers of exercises: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-    </>
-  )
- }
-
-
-
-  return (
-    <>
-      <Header course={course}/>
-      <Content/>
-      <Total/>
+    <p>{props.part1} {props.exercises1}</p>
+    <p>{props.part2} {props.exercises2}</p>
+    <p>{props.part3} {props.exercises3}</p>
     </>
   )
 }
+
+const Total = (props) => (
+  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+)
+
+const App = () => { 
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    return (
+      <div>
+           <Header course = {course}/>
+           <Content part1={part1} part2 = {part2} part3 = {part3}
+                    exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
+            <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/> 
+      </div>
+    )
+  }
+
 
 export default App;
