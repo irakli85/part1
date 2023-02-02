1cf65e8c55937f3bda00d3cfc82bedf01df2d0eb
irakli85 <iberidze38@gmail.com>
2023-02-02 10:57:08 +0400

    exercise 1.2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+
 const Header = (props) => (
   <h1>{props.course}</h1>
 )
 
+const Part = (props) => (
+  <p>{props.part} {props.exercises}</p>
+)
+
 const Content = (props) => {
   return(
-    <>
-    <p>{props.part1} {props.exercises1}</p>
-    <p>{props.part2} {props.exercises2}</p>
-    <p>{props.part3} {props.exercises3}</p>
-    </>
+    <div>
+      <Part part = {part1} exercises = {exercises1}/>
+      <Part part = {part2} exercises = {exercises2}/>
+      <Part part = {part3} exercises = {exercises3}/>
+    </div>
   )
 }
 
@@ -17,13 +30,7 @@ const Total = (props) => (
 )
 
 const App = () => { 
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    
 
     return (
       <div>
