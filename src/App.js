4c25763405886de07b7c24f2499eeb5faed76975
irakli85 <iberidze38@gmail.com>
2023-02-09 17:02:26 +0400

    exercises 1.6, 1.7

--- src/App.js
+++ src/App.js
@@ -1,61 +1,43 @@
+import { useState } from "react";
+
+const Heading = ({text}) => <h1>{text}</h1>
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Paragraph = ({text, count, text2}) => <p>{text} {count}{text2}</p>
+
 const App = () => { 
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const course = {
-    name: 'Half Stack application development',
-
-    parts: [
-        {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  } 
-
-  const Header = (props) =>{
-    console.log (props)
-    return (
-    <h1>{props.course}</h1>
-  )
-}
-
-  const Part = (props) => {
-    console.log(props)
-    return(
-    <p>{props.part} {props.exercises}</p>
-  )
-}
-  const Content = () => {
-    return(
-      <div>
-        <Part part = {course.parts[0].name} exercises = {course.parts[0].exercises}/>
-        <Part part = {course.parts[1].name} exercises = {course.parts[1].exercises}/>
-        <Part part = {course.parts[2].name} exercises = {course.parts[2].exercises}/>
-      </div>
-    )
+  const handleClickGood = () => {
+    setGood(good + 1)
   }
 
-  const Total = (props) => {
-    console.log(props)
-    return(
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
-}  
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
 
+  const handleClickBad = () => {
+    setBad( bad + 1)
+  }
+
+  let all = good + neutral + bad
+  let average = (good-bad) / all
+  let positive = (good/all)*100
+  
     return (
       <div>
-           <Header course = {course.name}/>
-           <Content />
-            <Total exercises1 = {course.parts[0].exercises} exercises2 = {course.parts[1].exercises} exercises3 = {course.parts[2].exercises}/>
+        <Heading text='give feedback'/>
+        <Button onClick={handleClickGood} text = 'good'/>
+        <Button onClick={handleClickNeutral} text = 'neutral'/>
+        <Button onClick={handleClickBad} text = 'bad'/>
+        <Heading text= 'statistics'/>
+        <Paragraph text='good' count = {good}/>
+        <Paragraph text='neutral' count = {neutral}/>
+        <Paragraph text='bad' count = {bad}/>
+        <Paragraph text='all' count = {all}/>
+        <Paragraph text='average' count = {isNaN(average) ? '0' : average}/>
+        <Paragraph text='positive' count ={isNaN(positive) ? '0' : positive} text2=' %'/>             
       </div>
     )
   }
