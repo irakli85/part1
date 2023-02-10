aafe68b1262f860f6f21c2eb097294bdc1507615
irakli85 <iberidze38@gmail.com>
2023-02-10 14:26:59 +0400

    exercise 1.12

--- src/App.js
+++ src/App.js
@@ -1,69 +1,37 @@
 import { useState } from "react";
-
-const Heading = ({text}) => <h1>{text}</h1>
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-const StatisticLine = ({text, value, percentage}) =>{
-  return(
-    <tr>
-      <td>{text}</td>
-      <td>{value}{percentage}</td>
-    </tr>
-  )
-}
-
-
+   
 
 const App = () => { 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [allFeedback, setAll] = useState(0)
 
-  const handleClickGood = () => {
-    setAll(allFeedback + 1)
-    setGood(good + 1)
-  }
+  const anecdotes = [
+    'If it hurts, do it more often.',
 
-  const handleClickNeutral = () => {
-    setAll(allFeedback + 1)
-    setNeutral(neutral + 1)
-  }
+    'Adding manpower to a late software project makes it later!',
 
-  const handleClickBad = () => {
-    setAll(allFeedback + 1)
-    setBad( bad + 1)
-  }
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
 
-  let all = good + neutral + bad
-  let average = ((good-bad) / all).toFixed(1)
-  let positive = ((good/all) * 100).toFixed(1)
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
 
-  const Statistics = () => {
-    if(allFeedback === 0){
-      return(
-        <p>No feedback given</p>
-      )
-    }
-    return(
-      <table>        
-        <StatisticLine text='good' value = {good}/>
-        <StatisticLine text='neutral' value = {neutral}/>
-        <StatisticLine text='bad' value = {bad}/>
-        <StatisticLine text='all' value = {all}/>
-        <StatisticLine text='average' value = {isNaN(average) ? '0' : average}/>
-        <StatisticLine text='positive' value ={isNaN(positive) ? '0' : positive} percentage=' %'/>
-      </table>  
-    )
-  }
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+
+    'The only way to go fast, is to go well.'
+  ]
   
+  const [selected, setSelected] = useState(0)  
+  
+
+  const handleClick = () => {
+    let randNum = Math.floor(Math.random() * 10) 
+    setSelected(randNum <= 6 ? randNum : 0)
+  } 
+
     return (
       <div>
-        <Heading text='give feedback'/>
-        <Button onClick={handleClickGood} text = 'good'/>
-        <Button onClick={handleClickNeutral} text = 'neutral'/>
-        <Button onClick={handleClickBad} text = 'bad'/>
-        <Heading text= 'statistics'/>
-        <Statistics/>          
+        <p>{anecdotes[selected]}</p>
+        <button onClick={handleClick}>next anecdote</button>
       </div>
     )
   }
