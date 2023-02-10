899eade09782763242b2b9ba79ab2d8d52dbc9d8
irakli85 <iberidze38@gmail.com>
2023-02-10 17:36:06 +0400

    half 1.13

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
 
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+
     'Premature optimization is the root of all evil.',
 
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
@@ -20,18 +21,29 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
   
-  const [selected, setSelected] = useState(0)  
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(0)
+  const [allVotes, setAllVotes] = useState([])  
   
 
-  const handleClick = () => {
+  const handleNext = () => {    
     let randNum = Math.floor(Math.random() * 10) 
-    setSelected(randNum <= 6 ? randNum : 0)
+    setSelected(randNum <= 7 ? randNum : 0)
+    console.log(selected)
   } 
+
+  const handleVote = () => {
+    setVote(vote + 1)
+    setAllVotes()
+    console.log(vote)
+  }
 
     return (
       <div>
         <p>{anecdotes[selected]}</p>
-        <button onClick={handleClick}>next anecdote</button>
+        <p> has {vote} votes</p>
+        <button onClick={handleVote}>votes</button>
+        <button onClick={handleNext}>next anecdote</button>
       </div>
     )
   }
